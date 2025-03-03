d8985f4b58f1d364a7df5f6594c228aff055104e
suprio-bhowmick <supriobhowmick2025@gmail.com>
2025-03-03 19:47:56 +0530

    Fixed serverless issue

--- api/index.js
+++ api/index.js
@@ -1,9 +1,11 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router("data.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
 
-module.exports = server;
+module.exports = (req, res) => {
+  server(req, res);
+};
